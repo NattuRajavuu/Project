6bf44bff8c4c19fb2587b4a69a0047bc017de1b9
Alwin <alwincb558@gmail.com>
2026-05-18 14:54:58 +0530

    new file

--- encapsulaton.py
+++ encapsulaton.py
@@ -8,4 +8,5 @@ class person:
 
 p1 = person("Alwin", 18)
 print(p1.name)
+# print(p1.age) this will print the name and age of the person object p1. The get_age method is added to demonstrate encapsulation, allowing access to the age attribute through a method rather than directly.
 print(p1.get_age())
